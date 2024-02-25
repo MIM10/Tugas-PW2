ee7418dfa40e611d0603999693d43833589ea02d
Abdullah Qaid Mu'aadz <107825990+MIM10@users.noreply.github.com>
2024-02-25 23:26:34 +0700

    update

--- P1/daftarNilaiMahasiswa.php
+++ P1/daftarNilaiMahasiswa.php
@@ -1,9 +1,9 @@
 <?php
     // Buat array asosiatif nilai
-    $nilai1 = ["id" => 1, "nim" => "01101", "uts" => 80, "uas" => 84, "tugas" => 78];
-    $nilai2 = ["id" => 2, "nim" => "01102", "uts" => 90, "uas" => 80, "tugas" => 88];
-    $nilai3 = ["id" => 3, "nim" => "01103", "uts" => 70, "uas" => 74, "tugas" => 70];
-    $nilai4 = ["id" => 4, "nim" => "01104", "uts" => 88, "uas" => 95, "tugas" => 80];
+    $nilai1 = ["id" => 1, "nama" => "Udin", "nim" => "0110290001", "uts" => 80, "uas" => 84, "tugas" => 78];
+    $nilai2 = ["id" => 2, "nama" => "Jamal", "nim" => "0110290002", "uts" => 90, "uas" => 80, "tugas" => 88];
+    $nilai3 = ["id" => 3, "nama" => "Dani", "nim" => "0110290003", "uts" => 70, "uas" => 74, "tugas" => 70];
+    $nilai4 = ["id" => 4, "nama" => "Benny", "nim" => "0110290004", "uts" => 88, "uas" => 95, "tugas" => 80];
 
     // Buat array multidimensi
     $kumpulan_nilai = [$nilai1, $nilai2, $nilai3, $nilai4];
@@ -18,23 +18,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous"> -->
 
     <title>Nilai Siswa</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        body {
+            display: flex;
+            justify-content: center;
+            height: 100dvh;
+        }
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            gap: 20px;
+        }
+        table {
+            width: 1000px;
+            border: 1px solid #ccc;
+        }
+        th,td {
+            padding: 10px;
+        }
+        .thead-dark {
+            background-color: #333;
+            color: #fff;
+        }
+    </style>
 </head>
 
 <body>
     <div class="container">
         <h1 class="text-center">Daftar Nilai Siswa</h1>
-        <table class="table">
+        <table border class="table">
             <thead class="thead-dark">
                 <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">NIM</th>
-                    <th scope="col">UTS</th>
-                    <th scope="col">UAS</th>
-                    <th scope="col">Tugas</th>
-                    <th scope="col">Nilai Akhir</th>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>NIM</th>
+                    <th>UTS</th>
+                    <th>UAS</th>
+                    <th>Tugas</th>
+                    <th>Nilai Akhir</th>
                 </tr>
             </thead>
             <tbody>
@@ -43,6 +74,7 @@
                     <tr>
                         <?php $nilai_akhir = ($nilai["uts"] + $nilai["uas"] + $nilai["tugas"]) / 3; ?>
                         <td><?= $nilai["id"]?></td>
+                        <td><?= $nilai["nama"]?></td>
                         <td><?= $nilai["nim"]?></td>
                         <td><?= $nilai["uts"]?></td>
                         <td><?= $nilai["uas"]?></td>
